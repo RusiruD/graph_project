1de30311094595946e2c35f6d52ab3f39ec8339e
RusiruD <rdha868@aucklanduni.ac.nz>
2023-12-08 14:39:46 +1300

    General-Added text field to primary fxml and removed button from secondary fxml

--- src/main/java/rusiru/project/PrimaryController.java
+++ src/main/java/rusiru/project/PrimaryController.java
@@ -2,8 +2,12 @@ package rusiru.project;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class PrimaryController {
+    @FXML Button submitBtn;
+    @FXML TextField numNodeTextField;
 
     @FXML
     private void switchToSecondary() throws IOException {
